608778e003120cc357ef01e9b88f094d8024997f
bumpincloudsson <hazelgrass46@gmail.com>
2023-06-10 16:06:40 -0500

    Rip, vid gone

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,3 +1,20 @@
+import { prisma } from "@/utils/db";
+import { auth, currentUser } from "@clerk/nextjs";
+
+const createNewUser = async () => {
+  const user = await currentUser();
+  const match = await prisma.user.findUnique({
+    where: { clerkId: user.id as string },
+  });
+
+  if (!match) {
+    const newUser = await prisma.user.create({
+      clerkId: user.id,
+      email: user.email,
+    });
+  }
+};
+
 const NewUser = () => {
   return <div>Hi New User</div>;
 };
